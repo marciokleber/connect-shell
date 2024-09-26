414b05d853b1f2fc50114a80ad20ddadcba9ea98
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2024-09-26 00:18:33 -0300

    Ajuste na estrutura do server e client

--- src/main/java/com/github/marciokleber/connectshell/ConnectShellApplication.java
+++ src/main/java/com/github/marciokleber/connectshell/ConnectShellApplication.java
@@ -1,21 +1,26 @@
 package com.github.marciokleber.connectshell;
 
+import com.github.marciokleber.connectshell.socket.SocketClient;
 import com.github.marciokleber.connectshell.socket.SocketServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class ConnectShellApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Aplicação iniciada com sucesso.");
+        SpringApplication.run(ConnectShellApplication.class, args);
 
         new Thread(() -> {
             SocketServer socketServer = new SocketServer(3000);  // Porta do servidor
             socketServer.start();
         }).start();
 
-        SpringApplication.run(ConnectShellApplication.class, args);
+        SocketClient socketClient = new SocketClient();
+        socketClient.connectToServer();
     }
 
 }
